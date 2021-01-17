b724b6ffba60741e6d10b89d45c6354eb7cc457c
chris <chr_zervas@hotmail.com>
2021-01-17 06:19:29 +0200

    finished edit-update price in My Products modal

--- front_end/fuel-app/src/components/modals/MyProductsModal.js
+++ front_end/fuel-app/src/components/modals/MyProductsModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Modal, ListGroup } from 'react-bootstrap';
+import { Button, Modal, ListGroup, FormControl } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import { axiosInstance } from '../axiosInstance';
 
@@ -8,6 +8,7 @@ export function MyProductsModal({ show, handleClose }) {
   const [gasStations, setGasStations] = useState(undefined);
   const [products, setProducts] = useState([]);
   const [selectedStation, setSelectedStation] = useState('');
+  const [newPrice, setNewPrice] = useState('');
 
 
   useEffect(() => {
@@ -45,7 +46,7 @@ export function MyProductsModal({ show, handleClose }) {
 
     getProducts();
     //rerender on login logout userdatacontext
-  }, [selectedStation]);
+  }, [selectedStation, newPrice]);
 
 
 
@@ -60,6 +61,7 @@ export function MyProductsModal({ show, handleClose }) {
           <Modal.Title>My Products</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+
           {gasStations !== undefined ?
             (<select onChange={(e) => {
               setSelectedStation(e.target.value);
@@ -75,12 +77,15 @@ export function MyProductsModal({ show, handleClose }) {
             <ListGroup>
               {products.map((product) => {
                 return (
-                  <ListGroup.Item key={product.productID}>
-                    Name: {product.fuelName}
-                    <br />
-                    Price: {product.fuelPrice}
-                    <Button>Change</Button>
-                  </ListGroup.Item>);
+
+                  <ProductItem
+                    key={product.productID}
+                    productID={product.productID}
+                    name={product.fuelName}
+                    price={product.fuelPrice}
+                    setPrice={setNewPrice} />
+
+                );
               })}
             </ListGroup>) : null}
 
@@ -91,4 +96,34 @@ export function MyProductsModal({ show, handleClose }) {
 
     </>
   );
+}
+
+
+function ProductItem({ productID, name, price, setPrice }) {
+
+  const [editable, setEditable] = useState(false);
+  const [changedValue, setChangedValue] = useState(price);
+
+  return (<ListGroup.Item >
+    Name: {name}
+    <br />
+    Price:{editable ? <input type="text" value={changedValue} size="4" onChange={(e) => { setChangedValue(e.target.value) }} /> : price}
+    <Button onClick={() => {
+      if (editable) {
+
+        axiosInstance.put("/pricedata/" + productID,
+          { newPrice: changedValue },
+          {
+            headers: { "Authorization": "Bearer " + localStorage.getItem('auth-token') }
+          }).then(res => {
+            setPrice(changedValue);
+          })
+
+      }
+
+      setEditable(!editable)
+
+    }}>{editable ? 'Update' : 'Edit'}</Button>
+  </ListGroup.Item>);
+
 }
